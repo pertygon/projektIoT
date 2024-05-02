6e0f02b81b7e782e0d67e2e2278746742ae58ab4
pertygon <50845875+pertygon@users.noreply.github.com>
2024-05-02 15:35:31 +0200

    Add files via upload

--- aplikacja.py
+++ aplikacja.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import (
     QPushButton, QRadioButton, QVBoxLayout, 
     QHBoxLayout, QVBoxLayout,QProgressBar,
     QLineEdit, QButtonGroup, QListWidget,
-    QFileDialog, QGroupBox
+    QFileDialog, QGroupBox, QMessageBox
 )
 import obliczenia
 import os
@@ -16,6 +16,11 @@ class oknoWyboru(QWidget):
         self.podgladLista = None
         self.cipGrupa = None
         self.cppGrupa = None
+        self.opcja = None
+        self.dlugosc = None
+        self.szerokosc = None
+        self.grubosc = None
+        self.ppkont = None
     def wyswietlPliki(self):
         try:
             self.dir_name = QFileDialog.getExistingDirectory(self, "Wybierz folder z danymi pomiarowymi")
@@ -26,12 +31,49 @@ class oknoWyboru(QWidget):
             pass
     def wyswietlCIP(self):
         self.cipGrupa.show()
+        self.opcja = 1
         self.cppGrupa.hide()
     def wyswietlCPP(self):
         self.cppGrupa.show()
+        self.opcja = 0
         self.cipGrupa.hide()
     def oblicz(self):
-        obliczenia.petlaObliczen(self.dir_name)
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Warning)
+        msgBox.setWindowTitle("Błąd")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        try:
+            self.grubosc = float(grProbki.text())
+            if self.opcja == 1:
+                self.dlugosc = float(dlProbki.text())
+                self.szerokosc = float(szProbki.text())
+            elif self.opcja == 0:
+                self.ppkont = float(ppKontaktu.text())
+        except:
+            msgBox.setText("Wystąpił błąd. Sprawdź czy wpisałeś liczby i użyłeś kropki. Można też użyć notacji naukowej.")
+            return msgBox.exec()
+        #czy wszystkie potrzebne pola zostaly uzupelnione?
+        if self.dir_name == "" or self.dir_name == None:
+            msgBox.setText("Źła ścieżka do plików")
+            return msgBox.exec()
+        elif self.opcja == None:
+            msgBox.setText("Nie wybrano sposobu pomiaru")
+            return msgBox.exec()
+        elif self.opcja == 1:
+            if self.dlugosc == None:
+                msgBox.setText("Pole długość próbki nie zostało uzupełnione")
+                return msgBox.exec()
+            if self.szerokosc == None:
+                msgBox.setText("Pole szerokość próbki nie zostało uzupełnione")
+                return msgBox.exec()
+        elif self.opcja == 0:
+            if self.ppkont == None:
+                msgBox.setText("Pole pole powierzchni kontaktu nie zostało uzupełnione")
+                return msgBox.exec()
+        elif self.grubosc == None:
+            msgBox.setText("Pole grubość próbki nie zostało uzupełnione")
+            return msgBox.exec()
+        obliczenia.petlaObliczen(self.dir_name,self.opcja,self.dlugosc,self.szerokosc,self.grubosc,self.ppkont)
     def interface(self):
 #ustawienia okna
         self.resize(800,400)
@@ -43,14 +85,13 @@ class oknoWyboru(QWidget):
         CPP = QRadioButton("CPP")
         radioGroup.addButton(CIP)
         radioGroup.addButton(CPP)
+        global szProbki, grProbki, dlProbki, ppKontaktu
         dlProbkiEt = QLabel("Długość próbki")
         dlProbki = QLineEdit()
         szProbkiEt = QLabel("Szerokość próbki")
         szProbki = QLineEdit()
-        grProbkiEt1 = QLabel("Grubość próbki")
-        grProbki1 = QLineEdit()
-        grProbkiEt2 = QLabel("Grubość próbki")
-        grProbki2 = QLineEdit()
+        grProbkiEt = QLabel("Grubość próbki")
+        grProbki = QLineEdit()
         ppKontaktuEt = QLabel("Pole powierzchni kontaktu")
         ppKontaktu = QLineEdit()
         podgladEtykieta = QLabel("Podgląd wybranych plików")
@@ -74,6 +115,7 @@ class oknoWyboru(QWidget):
         c2r1 = QHBoxLayout()
         c2r2 = QHBoxLayout()
         c2r3 = QHBoxLayout()
+        c2r4 = QHBoxLayout()
         #groupbox CIP
         g1m = QVBoxLayout()
         g1r1 = QHBoxLayout()
@@ -90,16 +132,12 @@ class oknoWyboru(QWidget):
         g1r1.addWidget(dlProbki)
         g1r1.addWidget(szProbkiEt)
         g1r1.addWidget(szProbki)
-        g1r2.addWidget(grProbkiEt1)
-        g1r2.addWidget(grProbki1)
         g1m.addLayout(g1r1)
         g1m.addLayout(g1r2)
         self.cipGrupa.setLayout(g1m)
         #groupbox CPP
         g2r1.addWidget(ppKontaktuEt)
         g2r1.addWidget(ppKontaktu)
-        g2r2.addWidget(grProbkiEt2)
-        g2r2.addWidget(grProbki2)
         g2m.addLayout(g2r1)
         g2m.addLayout(g2r2)
         self.cppGrupa.setLayout(g2m)
@@ -115,13 +153,15 @@ class oknoWyboru(QWidget):
         c2r1.addWidget(radioGroupBox)
         c2r3.addWidget(self.cipGrupa)
         c2r3.addWidget(self.cppGrupa)
-
+        c2r4.addWidget(grProbkiEt)
+        c2r4.addWidget(grProbki)
         column1.addLayout(c1r1)
         column1.addLayout(c1r2)
         column1.addLayout(c1r3)
         column2.addLayout(c2r1)
         column2.addLayout(c2r2)
         column2.addLayout(c2r3)
+        column2.addLayout(c2r4)
         row1.addLayout(column1)
         row1.addLayout(column2)
         row2.addWidget(obliczPrzycisk)
--- obliczenia.py
+++ obliczenia.py
@@ -2,30 +2,34 @@ import os
 import pandas as pd
 from pathlib import Path
 import wzory
-import glob 
-#Sprawdzic czy wszystkie biblioteki sa potrzebne
-#Zrobic mozliwosc zapisywania do excela
+import glob
+import natsort
 #Zrobic mozliwosc czytania z excela
+#nazwy plikow musza byc liczbami
+#dodac odpowiednie reagowanie na zla nazwe pliku
 def folderObliczone(sciezka):
     try:
         path = os.path.join(sciezka, "obliczone") 
         os.mkdir(path)
     except:
         pass
-def petlaObliczen(sciezka):
+def petlaObliczen(sciezka,opcja,dl,sz,gr,ppk):
     folderObliczone(sciezka)
     csv_files = glob.glob(os.path.join(sciezka, "*.csv"))
     for plik in csv_files:
-        print(plik)
-        obliczenia(sciezka,plik)
-def obliczenia(sciezka,plik):
+        obliczenia(sciezka,plik,opcja,dl,sz,gr,ppk)
+    do_wykresow(sciezka)
+def obliczenia(sciezka,plik,opcja,dl,sz,gr,ppk):
 #pododawac bloki try except do wykrycia bledow
 #czytanie danych
     nazwaPliku = Path(plik).stem
     df = pd.read_csv(plik,sep=';',encoding="utf-8")
     #czyszczenie danych
     df.columns = df.columns.str.strip()
-    df['Temp']=int(nazwaPliku)
+    try:
+        df['Temp'] = int(nazwaPliku)
+    except:
+        df['Temp'] = 0
     df.drop(df.filter(regex="Unname"),axis=1, inplace=True)
     df.replace(',','.',inplace = True)
     df['PHASe'] = df['PHASe'].str.replace(',','.').astype(float)
@@ -33,16 +37,57 @@ def obliczenia(sciezka,plik):
     df['D'] = df['D'].str.replace(',','.').astype(float)
     df['Rp'] = df['Rp'].str.replace(',','.').astype(float)
     #Tworzenie nowych kolumn i tworzenie obliczen
-    # DODAC TUTAJ PARAMETRY ZAMIAST LICZB I SPRAWDZIC WARUNEK
-    #if CIP.isChecked():
-    df['Konduktywnosc sigma'] = wzory.cip(wzory.Rho(df['Rp']),1,1)
-    #else"
-    #df['Konduktywnosc sigma'] = wzory.cpp(wzory.Rho(df['Rp']),1,1)
+    if opcja == 1:
+        df['Konduktywnosc sigma'] = wzory.cip(wzory.Rho(df['Rp']),gr*sz,dl)
+    else:
+        df['Konduktywnosc sigma'] = wzory.cpp(wzory.Rho(df['Rp']),gr,ppk)
     df['rho'] = wzory.Rho(df['Rp'])
     df['Tp w K'] = df['Temp'] + 273
     df['1000/Tp w K'] = 1000/df['Tp w K']
-    df['wspl. kond. alfa'] = wzory.wsplKonduktywnosci(10,12)
-    df['Re skladowa przenikalonsci dielekt.'] = wzory.rSkladowaPrzenikalnosci(df['Cp'],wzory.C0(1,1))
+    df['wspl. kond. alfa'] = wzory.wsplKonduktywnosci(df['Konduktywnosc sigma'],df['Freq'])
+    df['Re skladowa przenikalonsci dielekt.'] = wzory.rSkladowaPrzenikalnosci(df['Cp'],wzory.C0(ppk,gr))
     df['Im skladowa przenikalonsci dielekt.'] = wzory.iSkladowaPrzenikalnosci(df['Konduktywnosc sigma'],wzory.omega(df['Freq']))
     doZapisania = os.path.join(sciezka,'obliczone\\')
     df.to_excel(doZapisania+nazwaPliku+'.xlsx',index = False)
+def do_wykresow(sciezka):
+    """
+    stworz dataframe - my dla potrzebnych parametrow
+    otworz folder
+    czytaj plik po pliku 
+    zapisuj wartosci do plikow
+
+    """
+    wspAlfa = pd.DataFrame()
+    wspCp = pd.DataFrame()
+    wspEps = pd.DataFrame()
+    wspR = pd.DataFrame()
+    wspRo = pd.DataFrame()
+    wspSigma = pd.DataFrame()
+    wspTeta = pd.DataFrame()
+    wspTgDelta = pd.DataFrame()
+    pathWykresy = os.path.join(sciezka, "wykresy") 
+    pathObliczone = os.path.join(sciezka, "obliczone")
+    os.mkdir(pathWykresy)
+    xlxsPliki = glob.glob(os.path.join(pathObliczone, "*.xlsx"))
+    xlxsPliki = natsort.natsorted(xlxsPliki,reverse=False)
+    for plik in xlxsPliki:
+        nazwaPliku = Path(plik).stem
+        df = pd.read_excel(plik)
+        wspAlfa[nazwaPliku] = df['wspl. kond. alfa']
+        wspCp[nazwaPliku] = df['Cp']
+        #?????
+        wspEps[nazwaPliku] = 0
+        wspR[nazwaPliku] = df['Rp']
+        wspRo[nazwaPliku] = df['rho']
+        wspSigma[nazwaPliku] = df['Konduktywnosc sigma']
+        wspTeta[nazwaPliku] = df['PHASe']
+        wspTgDelta[nazwaPliku] = df['D']
+    wspAlfa.to_excel(pathWykresy+'\\'+'wsp alfa'+'.xlsx',index = False)
+    wspCp.to_excel(pathWykresy+'\\'+'wsp Cp'+'.xlsx',index = False)
+    wspEps.to_excel(pathWykresy+'\\'+'wsp Epsilon'+'.xlsx',index = False)
+    wspR.to_excel(pathWykresy+'\\'+'wsp R'+'.xlsx',index = False)
+    wspRo.to_excel(pathWykresy+'\\'+'wsp Ro'+'.xlsx',index = False)
+    wspSigma.to_excel(pathWykresy+'\\'+'wsp sigma'+'.xlsx',index = False)
+    wspTeta.to_excel(pathWykresy+'\\'+'wsp teta'+'.xlsx',index = False)
+    wspTgDelta.to_excel(pathWykresy+'\\'+'wsp tg delta'+'.xlsx',index = False)
+do_wykresow(r"D:\Michal\MoC 1.1\Ta=niew\Pomiary")
