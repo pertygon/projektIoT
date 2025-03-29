daef82c5d53d3f0a6a85c0e9612dd1b49881018b
pertygon <50845875+pertygon@users.noreply.github.com>
2025-03-29 01:11:14 +0100

    Add files via upload

--- wzory.py
+++ wzory.py
@@ -1,20 +1,20 @@
 import numpy as np
 #stale
-epsilon0 = 8.85e-14
+epsilon0 = 8.85e-12
 pi = np.pi
 #wzory
 def Rho(Rp):
     return 1/Rp
-def cip(rho,l,S):
-    return ((rho*l)/S)
-def cpp(rho,d,Pk):
-    return ((rho*d)/Pk)
+def cip(Rp,S,l):
+    return (l*1e-2)/(Rp*S*1e-4)
+def cpp(Rp,d,Pk):
+    return  (d*1e-2)/(Rp*Pk*1e-4)
 def wsplKonduktywnosci(sigma,f):
     return (np.log1p(sigma)/np.log1p(f))
 def rSkladowaPrzenikalnosci(Cp,C0):
     return Cp/C0
 def C0(A,d):
-    return epsilon0*(A/d)
+    return epsilon0*((A*1e-4)/(d*1e-2))
 def iSkladowaPrzenikalnosci(sigma,omega):
     return (sigma/(epsilon0*omega))
 def omega(f):
